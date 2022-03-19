5310e10d76137e1fda30b5aa1c1da5a8f5ae1668
oper <oper@mpifr.de>
2022-03-19 23:24:44 +0100

    make_initial script tracks based on 230G vex files present

--- scripts/make_initial_clocks.sh
+++ scripts/make_initial_clocks.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-pushd $SCRIPT_DIR
+pushd $SCRIPT_DIR 2>&1 > /dev/null
+
+expts=""
+VEXDIR=../observed_vex/
+for vexfile in `ls -1 $VEXDIR/*.vex`; do
+    EXPT=${vexfile%.vex}
+    expts="$expts ${EXPT##*/}"
+done
 
 # 230G
-for expt in e22j25 e22j26; do
+for expt in $expts; do
 	for band in band1 band2 band3 band4; do
 		clockfile=../templates/230G/$band/clocks_$expt.dat
 		if [[ ! -f $clockfile ]]; then
 			echo "Making empty $clockfile"
 			echo "CLOCK" > $clockfile
 			echo "# Station ID    Delay (usec)    Rate (usec)" >> $clockfile
+		else
+			# echo "Keeping existing $clockfile"
 		fi
 	done
 done
 
-# 345G
-#for expt in e21f19; do
-#	for band in band1 band2 band3 band4; do
-#		clockfile=../templates/345G/$band/clocks_$expt.dat
-#		if [[ ! -f $clockfile ]]; then
-#			echo "Making empty $clockfile"
-#			echo "CLOCK" > $clockfile
-#			echo "# Station ID    Delay (usec)    Rate (usec)" >> $clockfile
-#		fi
-#	done
-#done
-
-popd
-
+popd 2>&1 > /dev/null
--- scripts/make_initial_notes.sh
+++ scripts/make_initial_notes.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-pushd $SCRIPT_DIR
+pushd $SCRIPT_DIR   2>&1 > /dev/null
 
-for expt in e22j25 e22j26; do
+expts=""
+VEXDIR=../observed_vex/
+for vexfile in `ls -1 $VEXDIR/*.vex`; do
+    EXPT=${vexfile%.vex}
+    expts="$expts ${EXPT##*/}"
+done
+
+for expt in $expts; do
 	for band in 1 2 3 4; do
 		notesfile=../templates/230G/band$band/notes_$expt.v2d
 		if [[ ! -f $notesfile ]]; then
 			echo "Making initial $notesfile"
 			echo "# Track $expt band $band" > $notesfile
 			echo "# Notes: ..." >> $notesfile
+		else
+			# echo "Keeping existing $notesfile"
 		fi
 	done
 done
 
-#for expt in e21f19; do
-#	for band in 1 2 3 4; do
-#		notesfile=../templates/345G/band$band/notes_$expt.v2d
-#		if [[ ! -f $notesfile ]]; then
-#			echo "Making initial $notesfile"
-#			echo "# Track $expt band $band" > $notesfile
-#			echo "# Notes: ..." >> $notesfile
-#		fi
-#	done
-#done
+popd   2>&1 > /dev/null
+
