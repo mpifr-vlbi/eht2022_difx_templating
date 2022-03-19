4eee52e90f4cfe19e532e164c43e75d2e985350b
oper <oper@mpifr.de>
2022-03-19 23:42:49 +0100

    add g18 b19 c20 to create_vex_EOPs.sh

--- scripts/create_vex_EOPs.sh
+++ scripts/create_vex_EOPs.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#
-# e22j25.vex:     exper_nominal_start=2022y025d04h10m00s;
-# e22j26.vex:     exper_nominal_start=2022y026d04h06m00s;
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 OUTDIR=../templates/common_sections/
 
-pushd $SCRIPT_DIR
+pushd $SCRIPT_DIR  2>&1 > /dev/null
 
 ./geteop.pl 2022-23  5 $OUTDIR/eop_e22j25.vex
 ./geteop.pl 2022-24  5 $OUTDIR/eop_e22j26.vex
 
-popd
+./geteop.pl 2022-74  5 $OUTDIR/eop_e22g18.vex
+./geteop.pl 2022-76  5 $OUTDIR/eop_e22b19.vex
+./geteop.pl 2022-77  5 $OUTDIR/eop_e22c20.vex
+
+popd  2>&1 > /dev/null
 
